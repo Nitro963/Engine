1694ca929f9ff73e2c013da57bc63beeb63e3f6a
Nitro963 <adnankattan9@gmail.com>
2019-03-17 19:19:40 +0200

    Started Building The Physics Engine

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -1,27 +1,6 @@
-#include <GLFW/glfw3.h>
+#include <iostream>
 
-int main(void){
-	GLFWwindow* window;
-
-	if (!glfwInit())
-		return -1;
-
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-	if (!window){
-		glfwTerminate();
-		return -1;
-	}
-	glfwMakeContextCurrent(window);
-
-	while (!glfwWindowShouldClose(window)){
-		glClear(GL_COLOR_BUFFER_BIT);
-
-
-		glfwSwapBuffers(window);
-
-		glfwPollEvents();
-	}
-
-	glfwTerminate();
-	return 0;
+int main() {
+	std::cout << "Fuck Open GL\n";
+	system("pause");
 }
